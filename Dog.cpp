9666323a0cabc63ad8af20a80f15e62f952f9f3a
saveliiiy <43163704+saveliiiy@users.noreply.github.com>
2018-09-11 11:30:57 +0300

    Create Dog.cpp

---
+++ Dog.cpp
@@ -0,0 +1,57 @@
+#include "Dog.h"
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+Dog::Dog() {
+	cout << "Порода\n";
+	cin >> breed;
+	cout << "Окрас\n";
+	cin >> color;
+	cout << "Кличка\n";
+	cin >> name;
+	cout << "Пол\n";
+	cin >> pol;
+	cout << "Дата рождения\n";
+	cin >> date_of_birth;
+	cout << "Размеры\n";
+	cin >> dimensions;
+	cout << "Имя владельца \n";
+	cin >> owner_name;
+	cout << "Наименование питомника\n";
+	cin >> nursery_name;
+	cout << "Характер\n";
+	cin >> character;
+}
+
+void Dog::Vivod_in_not_fail() {
+	cout << endl << "Порода - " << breed << endl;
+	cout << "Окрас - " << color << endl;
+	cout << "Кличка - " << name << endl;
+	cout << "Пол - " << pol << endl;
+	cout << "Дата рождения - " << date_of_birth << endl;
+	cout << "Размеры - " << dimensions << endl;
+	cout << "Имя владельца - " << owner_name << endl;
+	cout << "Наименование питомника - " << nursery_name << endl;
+	cout << "Характер - " << character << endl;
+}
+
+void Dog::Vivod_in_fail() {
+	ofstream file1("Animals.txt", ios_base::app);
+	file1 << "====================" << endl;
+	file1 << "Собака" << endl;
+file1 << "Порода - " << breed << endl;
+file1 << "Окрас - " << color << endl;
+file1 << "Кличка - " << name << endl;
+file1 << "Пол - " << pol << endl;
+file1 << "Дата рождения - " << date_of_birth << endl;
+file1 << "Размеры - " << dimensions << endl;
+file1 << "Имя владельца - " << owner_name << endl;
+file1 << "Наименование питомника - " << nursery_name << endl;
+file1 << "Характер - " << character << endl;
+	file1 << "====================" << endl;
+	file1 << "\n\n";
+	file1.close();
+}
+Dog::~Dog() {}
